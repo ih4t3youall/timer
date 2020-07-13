f9302e1e3a1078db8534dea4d33cae0a30447fee
Juan Martin Lequerica <juan.martin.lequerica@jmlequerica-ch--ul>
2020-07-13 10:13:31 -0500

    now you can put a message

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from playsound import playsound
 import sys
 import time
 
+message = ''
 
 if len(sys.argv) < 2:
     print('you need to pass as a parameter an integer, will be the amount of minutes.')
     exit()
+
+if len(sys.argv) == 3:
+    message = sys.argv[2]
 
 try:
     sleep_time = int(sys.argv[1])
@@ -14,5 +18,6 @@ except ValueError:
     print('The number should be an integer')
     exit()
 
-time.sleep(sleep_time*60)
+time.sleep(sleep_time * 60)
 playsound('alarm.mp3')
+print(str(message))
